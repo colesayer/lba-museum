40af2af5ec3a20950f2630a695b36d0c5e2618a9
colesayer <colesayershapiro@gmail.com>
2018-10-20 17:13:36 -0400

    finished implementing fake api

--- src/homepage.js
+++ src/homepage.js
@@ -5,8 +5,10 @@ class Homepage{
 
   fetchContent(){
     const homepageContent = this.api.fetchHomepage()
-    this.initializeHeader(homepageContent.header)
-    this.initializeEvents(homepageContent.events)
+    const {header, events, collection} = homepageContent
+    this.initializeHeader(header)
+    this.initializeEvents(events)
+    this.initializeCollection(collection)
   }
 
   initializeHeader(header){
@@ -39,6 +41,36 @@ class Homepage{
         <date>${date}</date>
       `
       eventsList.appendChild(eventCard)
+    }
+  }
+
+  initializeCollection(collection){
+    const collectionList = document.getElementById('collection-list')
+
+    for(let item of collection){
+      const {artist, title, date, footnote, image, imageAlt} = item
+      const collectionCard = document.createElement("SECTION")
+      collectionCard.className = "collection-card"
+      collectionCard.innerHTML = `
+        <div class="collection-card-image">
+          <img src="${image}" alt=${imageAlt}>
+          <span class="image-overlay"></span>
+        </div>
+        <h1>${artist}</h1>
+        <h2>${title}, ${date}</h2>
+      `
+      if(footnote){
+        const footnoteContainer = document.createElement("DIV")
+        footnoteContainer.className = "collection-card-footnote"
+        footnoteContainer.innerHTML = `
+          <div class="collection-bullet"></div>
+          <h3>${footnote}</h3>
+        `
+        collectionCard.appendChild(footnoteContainer)
+      }
+
+      collectionList.appendChild(collectionCard)
+
     }
   }
 }
